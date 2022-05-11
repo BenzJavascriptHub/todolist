5d2dbef74789ec9b5fbcd208482b6d35f3c92ff4
lenrich <lenrich1403@gmail.com>
2022-05-11 12:34:02 +0800

    add task project service

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ const tick = () => {
 };
 
 setInterval(tick, 1000);// 定時器(定時執行函數, 毫秒(1000毫秒=1秒))
+
+const form = document.querySelector('.add-task');
+const addTask = document.querySelector('#add-task');
+const tasks = document.querySelector('#tasks');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // 防止頁面重新刷新
+  console.log(addTask.value);
+  tasks.innerHTML += `<div class="to-do-task">
+  <input type="checkbox" class="check-task"/>
+  <p class="task-content">${addTask.value}</p>
+</div>`;
+});
+
