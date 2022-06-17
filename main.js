477e9d1b24547d3b94643ee85dcfdfe010e368b2
lenrich <lenrich1403@gmail.com>
2022-06-17 17:39:03 +0800

    loading view

---
+++ main.js
@@ -0,0 +1,16 @@
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.display = 'block';
+        // 計時50毫秒後，才轉成opacity = 1
+        setTimeout(() => { main.style.opacity = 1 }, 50)
+
+    }, 2500)
+}
+
+init();
